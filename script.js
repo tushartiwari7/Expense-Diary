72b6c2baba7a15b281e70af23096abe120c839dd
tushartiwari0172@gmail.com <tushartiwari0172@gmail.com>
2021-03-25 17:25:07 +0530

    bill saved on localStorage also

--- script.js
+++ script.js
@@ -4,13 +4,14 @@
 	const description = document.querySelector("#primaryDesc");
 	const showData = document.querySelector("#putData");
 	const element = document.querySelector("#add");
-	// document is our whole page which we are viewing and query selector is used to fetch the button which has id as add. we use # in Js for id's just like we use . in css for classes. element is a variable used to store the information of thetag related to mentioned id (in this case the button statement is stored under element. In JS ES6 there are only two datatypes for variables i.e. const and let.
 	element.addEventListener("click", addition, false); 
-	//add event listener will add a functionality to our element i.e. button of click so whenever a click is encountered it will make a call to function addition
 	if(localStorage.getItem('data')!== null) {
 		processing();
 	}
-	headingEl.textContent = 0;
+	if(localStorage.getItem('bill')!== null)
+		headingEl.textContent = localStorage.getItem('bill');
+	else
+		headingEl.textContent = 0;
 	let base = 0;
 	let allinfo = [];
 	function addition() {
@@ -28,17 +29,31 @@
 		allinfo.push(currentEl);
 		localStorage.setItem('data',JSON.stringify(allinfo));
 		document.expenseInputForm.reset();	// to clear the input form after one successful submit iteration		
-		const primarynum = parseInt(currentEl.rupees, 10);
-		base = base + primarynum;
-		headingEl.textContent = base;
+		addToBill(currentEl.rupees);
 		processing();
 	}
-	function processing() {
-		let arr = JSON.parse(localStorage.getItem('data'));
-		const allinfoHTML = arr.map(expense => createItemList(expense.description, expense.rupees,new Date(expense.thismoment)));
-		const joinedallinfoHTML = allinfoHTML.join("");
-		showData.innerHTML = joinedallinfoHTML;
-	}
+function addToBill(price) {
+	const primarynum = parseInt(price, 10);
+	var billValue = JSON.parse(localStorage.getItem('bill'));
+	billValue = billValue + primarynum;
+	console.log(billValue);
+	localStorage.setItem('bill',JSON.stringify(billValue));
+	headingEl.textContent = billValue;
+}
+
+function deleteFromBill( price ) {
+	base = base - price;
+	var billValue = JSON.parse(localStorage.getItem('bill'));
+	billValue = billValue - price;
+	localStorage.setItem('bill',JSON.stringify(billValue));
+	headingEl.textContent = billValue;
+}
+function processing() {
+	let arr = JSON.parse(localStorage.getItem('data'));
+	const allinfoHTML = arr.map(expense => createItemList(expense.description, expense.rupees,new Date(expense.thismoment)));
+	const joinedallinfoHTML = allinfoHTML.join("");
+	showData.innerHTML = joinedallinfoHTML;
+}
 
 		function createItemList( description, rupees, thismoment) {
 			return `
@@ -64,11 +79,6 @@
 				`;
 		}	
 
-		function deleteItemFromTotal( Item ) {
-			base = base - Item.rupees;
-			headingEl.textContent = base;
-		}
-
 		function deleteItem(timeCreated ) {
 			let arr = JSON.parse(localStorage.getItem('data'));
 			arr.forEach(
@@ -76,7 +86,7 @@
 					var fetchedTime = new Date(element.thismoment);
 					var fetchedTimeVal = fetchedTime.valueOf();
 					if(fetchedTimeVal === timeCreated) {
-						deleteItemFromTotal(element);
+						deleteFromBill(element.rupees);
 						const indexOfDelEl = arr.indexOf(element);
 						arr.splice(indexOfDelEl,1);
 					}
